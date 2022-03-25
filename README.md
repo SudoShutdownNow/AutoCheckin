cf3ac60ca7fb2613299c5cd6955310e926765694
SudoShutdownNow <zhaozeyu8888@outlook.com>
2022-03-25 16:53:22 +0800

    新增日志记录、推送结果功能

--- README.md
+++ README.md
@@ -1,57 +1,95 @@
-# AutoCheckin 学习通自动签到
+# AutoCheckin 学习通自动签到-服务器版
+Fork自：https://github.com/Huangyan0804/AutoCheckin
 
 **！！！支持普通签到，手势签到，二维码签到，拍照签到，位置签到**
 学习通自动签到，针对早起不能学生，**需要自行提供参数**。
 
+在原有签到功能基础上：
+* 优化 修改调用参数，将签到与登录功能分离
+* 优化 将登陆过期、未登录按异常处理
+* 优化 将签到参数从代码中移除，保存在config.json中
+* 新增 通过WxPusher推送执行结果
+* 新增 日志记录功能
+
+
 **已知cookie的有效时间为一个月，请放心使用**
 
-**登录方式**：首次登录使用二维码登录，登录成功后，自动保存cookie，**下次无需重新登录**
+**登录方式**：首次登录使用二维码登录，登录成功后，自动保存cookie，**下次无需重新登录，直到登陆过期**
 
 ## 一、参数修改：
 
-### 1.课程参数：
+请在config.json中修改以下参数
 
-```python
-   course_list = [
+### 1.学生姓名（选填）
+
+将用于推送签到结果和日志记录，便于多人共用一个服务器时定位问题
+```json
+  "stuName": "您的姓名"
+```
+### 2.课程参数：
+
+```json
+   "course_list": [
         {
-            'name':  # 你的姓名
-            'url':  # 课程的任务页面/活动首页
-            'course_name':  # 课程名称，用于单课程签到指令和提示输出
+            'url': "" // 课程的任务页面/活动首页
+            'course_name':  // 课程名称，用于单课程签到指令和提示输出
         }
     ]
 ```
-
 #### 课程URL为进入课程后右上角的任务
 
 ![1](images/2020-03-15-160930.png)
 
+### 3.地址参数：
+将用于位置签到时提交的地址
 
-
-### 2.位置信息
-
-```python
-    address = {
-        "latitude": "-1",  # 纬度
-        'longitude': "-1",  # 经度
-        'addr': "",  # 位置名称
-        'ifTiJiao': "0"  # 是否开启提交位置信息，'0'关闭, '1'开启
-    }
+```json
+ "address": {
+    "latitude": "-1",//纬度
+    "longitude": "-1",//经度
+    "addr": "",//地址文字
+    "ifTiJiao": "0"//是否提交地址
+  }
 ```
 
-### 3.拍照签到的图片
-
+### 4.拍照签到的图片
 请在该文件的目录下存放名字为up_img.jpg的图片
+如有拍照签到会自动上传该图片，否则会自动上传wyz！
 
-如有拍照签到会自动上传该图片，**否则会自动上传wyz照片**！
+### 5.WxPusher参数
+WxPusher是一个建立在微信上的推送API，官方网站：https://wxpusher.zjiecode.com/
+
+在这里可以设置WxPusher相关参数
+
+```json
+  "push_config": {"app_token": "AT_",//WxPusher的token
+               "push_UID": "UID_",//接收消息的WxPusher UID
+               "enable_push": true}//是否启用推送
+```
+
+### 6.日志保存设置
+```json
+"log_config": {
+    "log_dir": "./checkin.log",//日志保存位置
+    "enable_log": true//开启日志功能
+  }
+```
+
 
 ## 二、执行代码方式
 
 分所有课程检测和单课程检测：
 
+- 登陆方法
+
+  ```bash
+  python3 checkin.py -l
+  ```
+
 - 单课程签到调用方法：
 
   ```bash
-  python3 checkin.py course_name
+  python3 checkin.py -c course_name
   ```
 
   **curse_name为上面修改的参数中curse_list里的curse_name**
@@ -59,7 +97,7 @@
 - 所有课程签到调用方法：
 
   ```bash
-  python3 checkin.py
+  python3 checkin.py -a
   ```
 
   检测所有课程是否有签到任务。慎用，使用次数过多可能会被学习通拉入黑名单。
--- checkin.py
+++ checkin.py
@@ -9,10 +9,19 @@ import re
 from urllib.parse import quote
 import http.cookiejar as HC
 from PIL import Image
+import json
+
+config = json.load(open("config.json", "r", encoding="UTF-8"))
+#print(config)
+push_config = config['push_config']
+stuName = config['stuName']
+course_list = config['course_list']
+address = config['address']
+log_config = config['log_config']
 
 """
-
-    需要修改的地方, 请在下方代码处修改：
+    
+    需要修改的地方, 请在config.json修改：
     1.课程参数：
     course_list = [
         {
@@ -35,22 +44,6 @@ from PIL import Image
     如有拍照签到会自动上传该图片，否则会自动上传wyz！
 
 """
-
-# 需要签到的课程列表
-course_list = [
-    {
-        'name': "",  # 姓名
-        'url': '',
-        'course_name': ''
-    }
-]
-# 编辑位置信息
-address = {
-    "latitude": "-1",  # 纬度
-    'longitude': "-1",  # 经度
-    'addr': "",  # 位置名称
-    'ifTiJiao': "0"  # 是否开启提交位置信息，默认关闭
-}
 
 # <=========我是分割线========>
 
@@ -143,7 +136,7 @@ def re_login(login_headers):
     login_status = get_login_code(login_headers)
     if login_status['status']:
         print("登录成功")
-        r_session.cookies.save()
+        r_session.cookies.save()  # 将当前cookie保存上
     else:
         if login_status['type'] == '4':
             print("登录失败," + login_status['mes'] + "却未登录")
@@ -162,18 +155,22 @@ def login():
         'Referer': 'https://passport2.chaoxing.com/login?fid=2182&refer=http://i.mooc.chaoxing.com',
         'Origin': 'https://passport2.chaoxing.com',
     }
-    r_session.cookies = HC.LWPCookieJar(filename='cookies')
+    r_session.cookies = HC.LWPCookieJar(filename='cookies')  # 读取或生成一个cookie
 
     try:
         r_session.cookies.load(ignore_discard=True)
         if not check_is_login():
             print('cookie信息已过期，请重新登录')
-            re_login(login_headers)
+            write_log("登陆已过期")
+            raise Exception("{} 登录已过期".format(stuName))
+            # re_login(login_headers)
         else:
             print('已登录')
+            write_log("已登录")
     except:
         print('请登录')
-        re_login(login_headers)
+        raise Exception("{} 未登录".format(stuName))
+        # re_login(login_headers)
 
 
 def upload_img(filename):
@@ -294,45 +291,75 @@ def tphoto_check(base_url):
 
 def check_in():
     """post 签到"""
+    allRes = []  # 所有签到任务的结果
     if not active_list:
         print("没有签到任务")
-        return
+        write_log("没有签到任务")
+        return allRes
     # 循环签到各个任务
     for active_id in active_list:
+        res = {"type": "未知", "success": False}  # 单个签到任务的结果
         post_data['activeId'] = active_id
         base_url = 'https://mobilelearn.chaoxing.com'
         # 打开签到页
         response = normal_check(base_url)
         if re.findall(r'签到成功', response.text):
             print("==========>%s已签到成功" % active_id)
+            write_log("==========>%s已签到成功" % active_id)
+            res = {"type": "普通签到", "success": True}
+
         elif re.findall(r'手势图案', response.text):
             # 手势签到
+            res['type'] = "手势签到"
             response = hand_check(base_url)
             if re.findall(r'签到成功', response.text):
                 print("==========>%s手势签到成功" % active_id)
+                write_log("==========>%s手势签到成功" % active_id)
+                res['success'] = True
+
+
         elif re.findall(r'手机扫码', response.text):
             # 二维码签到
+            res['type'] = "二维码签到"
             response = qcode_check(base_url)
             if re.findall(r'success', response.text):
                 print("==========>%s二维码签到成功" % active_id)
+                write_log("==========>%s二维码签到成功" % active_id)
+                res['success'] = True
+
         elif re.findall(r'位置信息', response.text):
+            # 位置签到
+            res['type'] = "位置签到"
             response = addr_check(base_url)
             if re.findall(r'success', response.text):
                 print("==========>%s位置签到成功" % active_id)
+                write_log("==========>%s位置签到成功" % active_id)
+                res['success'] = True
+
         elif re.findall(r'手机拍照', response.text):
+            # 拍照签到
+            res['type'] = "拍照签到"
             response = tphoto_check(base_url)
             if re.findall(r'success', response.text):
                 print("==========>%s拍照签到成功" % active_id)
+                write_log("==========>%s拍照签到成功" % active_id)
+                res['success'] = True
         else:
             print('==========>%s签到失败,未知原因，详情请查看%s.html' % (active_id, active_id))
+            write_log('==========>%s签到失败,未知原因，详情请查看%s.html' % (active_id, active_id))
             save_html(response.text, active_id)
 
+        allRes.append(res)
         time.sleep(3)
+
+    return allRes
 
 
 def open_course_page(course):
     """打开课程活动页"""
+    course_name = course['course_name']
     print('正在检查%s课程签到任务' % course['course_name'])
+    write_log('正在检查%s课程签到任务' % course['course_name'])
     current_time = str(time.strftime("%m-%d %H:%M:%S", time.localtime()))
     request_url = course['url']
     response = r_session.get(url=request_url, headers=header)
@@ -340,27 +367,138 @@ def open_course_page(course):
     active_list.clear()  # 获得id前清空
     get_post_data(text)  # 获取post_data
     get_active_id(text)  # 获取签到任务
-    check_in()  # 签到
+    check_in_res = check_in()  # 签到
 
+    # 检验并回传签到结果
+    if len(check_in_res) == 0:
+        push_result(course_name, True, "该课程当前没有签到任务")
+        write_log(course_name + " 没有签到任务")
+        return
+    else:
+        for res in check_in_res:
+            if res['success']:
+                push_result(course_name, True, "已完成以下签到: \n{}".format(json.dumps(check_in_res)))
+                return
+
+        push_result(course_name, False, "以下签到均失败: \n{}".format(json.dumps(check_in_res)))
+
+
+def push_result(course_name: str, success: bool, message: str):
+    title = "{} {} 签到{}".format(stuName, course_name, "成功" if success else "失败")
+    push_body = {"appToken": push_config['app_token'],
+                 "summary": title,
+                 "content": message,
+                 "contentType": 1,
+                 "uids": [push_config['push_UID']]}
+    push_header = {"Content-Type": "application/json"}
+    if push_config['enable_push']:
+        requests.post(url="http://wxpusher.zjiecode.com/api/send/message", data=json.dumps(push_body),
+                      headers=push_header)
+
+def write_log(context):
+    if log_config['enable_log']:
+        try:
+            log_file = open(log_config['log_dir'], "a+", encoding="UTF-8")
+            current_time = str(time.strftime("%y-%m-%d %H:%M:%S", time.localtime()))
+            log_file.write("{}\t{}\n".format(current_time, context))
+            log_file.close()
+        except Exception as e:
+            print("Log保存出错: " + str(e))
 
 def main():
-    if len(sys.argv) > 2:
-        print('请输入正确的参数')
-    elif len(sys.argv) == 1:
+    #print(sys.argv)
+    '''
+    Parameter:
+    -l Login 登录或重新登陆
+    -a All 签到所有课程(可能封号，谨慎使用)
+    -c [课程名] 签到指定课程
+    '''
+
+    course_name = "未知课程"
+
+    try:
+        #登录
+        if len(sys.argv) == 1 or len(sys.argv) > 3:
+            raise Exception("参数错误")
+
+        elif sys.argv[1] == "-l":
+            write_log("执行登录")
+            login_headers = {
+                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                              'Chrome/80.0.3987.122 Safari/537.36',
+                'Upgrade-Insecure-Requests': '1',
+                'Host': 'passport2.chaoxing.com',
+                'Referer': 'https://passport2.chaoxing.com/login?fid=2182&refer=http://i.mooc.chaoxing.com',
+                'Origin': 'https://passport2.chaoxing.com',
+            }
+            r_session.cookies = HC.LWPCookieJar(filename='cookies')  # 读取或生成一个cookie
+            re_login(login_headers)
+
+        elif sys.argv[1] == "-a":
+            write_log("签到所有课程")
+            #遍历所有课程
+            login()
+            for course in course_list:
+                open_course_page(course)
+                print('-' * 50)
+                time.sleep(5)
+
+        elif sys.argv[1] == "-c":
+            write_log("签到{} 课程".format(sys.argv[2]))
+            #签到指定课程
+            if len(sys.argv) != 3:
+                raise Exception("参数错误")
+            course_name = sys.argv[2]
+            for course in course_list:
+                if course['course_name'] == course_name:
+                    login()
+                    open_course_page(course)
+                    break
+
+        else:
+            raise Exception ("参数错误")
+
+    except Exception as e:
+        push_result(course_name, False, str(e))
+        print(e)
+
+def test_main():
+    course_name = "未知课程"
+    # 登录
+    if len(sys.argv) == 1 or len(sys.argv) > 3:
+        raise Exception("参数错误")
+
+    elif sys.argv[1] == "-l":
+        login_headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/80.0.3987.122 Safari/537.36',
+            'Upgrade-Insecure-Requests': '1',
+            'Host': 'passport2.chaoxing.com',
+            'Referer': 'https://passport2.chaoxing.com/login?fid=2182&refer=http://i.mooc.chaoxing.com',
+            'Origin': 'https://passport2.chaoxing.com',
+        }
+        r_session.cookies = HC.LWPCookieJar(filename='cookies')  # 读取或生成一个cookie
+        re_login(login_headers)
+
+    elif sys.argv[1] == "-a":
         # 遍历所有课程
         login()
         for course in course_list:
             open_course_page(course)
             print('-' * 50)
             time.sleep(5)
-    elif len(sys.argv) == 2:
-        course_name = sys.argv[1]
+
+    elif sys.argv[1] == "-c":
+        # 签到指定课程
+        if len(sys.argv) != 3:
+            raise Exception("参数错误")
+        course_name = sys.argv[2]
         for course in course_list:
             if course['course_name'] == course_name:
                 login()
                 open_course_page(course)
                 break
 
-
 if __name__ == '__main__':
     main()
+    # push_result("中国传统戏曲鉴赏", False, "波波是clj")
